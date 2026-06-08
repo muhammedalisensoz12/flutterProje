dcc81626333635561e0bf16efc9c896cd7db5972
muhammedalisensoz12 <muhammedalisensoz12@gmail.com>
2026-06-08 10:48:53 +0300

    kodda ufak eklemeler yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,26 @@ void main() {
   runApp(Uygulamam());
 }
 
+
 class Uygulamam extends StatelessWidget {
   const Uygulamam({super.key});
+  
+  // Uygulama sabitleri
+ static const String appName = 'Final Ödev';
+ static const String version = '1.0.0';
+ // Öğrenci onay kodu - silmeyiniz
+ final String _ogrenciOnayKodu = "MOBIL2026";
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Anasayfa());
-  }
+@override
+ Widget build(BuildContext context) {
+ return MaterialApp(
+ title: appName,
+ theme: ThemeData(
+ primarySwatch: Colors.blue,
+ useMaterial3: true,
+ ),
+ home: const Anasayfa(),
+ );
+ }
 }
+ // Bu uygulama Mobil Programlama dersi kapsamında geliştirilmiştir
